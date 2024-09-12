bdcfe2c2bac3912d293e450ca15c894c3306809c
nevas <rashevco@gmail.com>
2024-09-12 21:59:03 +0300

    almost working

---
+++ app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("products-container");
+
+    products.forEach(product => {
+        const productElement = document.createElement("div");
+        productElement.classList.add("product");
+
+        // Додаємо зображення товару
+        const img = document.createElement("img");
+        img.src = `photos/${product.photo}`;
+        img.alt = product.name;
+
+        // Назва товару
+        const title = docurment.createElement("h2");
+        title.textContent = product.name;
+
+        // Ціна товару 
+        const price = document.createElement("p");
+        price.classList.add("price");
+        price.textContent = `${product.price} грн`;
+
+        // Додаємо елементи в контейнер товару
+        productElement.appendChild(img);
+        productElement.appendChild(title);
+        productElement.appendChild(price);
+
+        // Додаємо товар у контейнер сторінки
+        container.appendChild(productElement);
+    });
+});
