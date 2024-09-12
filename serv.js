b5b2237eeaef3094c933a731c69cb71314056d48
nevas <rashevco@gmail.com>
2024-09-12 21:44:29 +0300

    add serv

---
+++ serv.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        id: 1,
+        name: "Смартфон",
+        price: 15000,
+        photo: "smartphone.jpg"
+    },
+    {
+        id: 2,
+        name: "Ноутбук",
+        price: 35000,
+        photo: "laptop.jpg"
+    },
+    {
+        id: 3,
+        name: "Навушники",
+        price: 2500,
+        photo: "headphones.jpg"
+    }
+];
